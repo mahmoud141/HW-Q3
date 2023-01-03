b5e056f39ed16b1ca95f80b447a1aae15bec07e1
ENG MA <mmaamm72@windowslive.com>
2023-01-03 18:40:58 +0200

    FIRST vERSION

---
+++ HW4 Q1.py
@@ -0,0 +1,7 @@
+numbers = [12, 75, 145, 150, 180, 525, 50]
+
+
+for number in numbers:
+    if number % 5 == 0 and 150 <= number <= 500:
+        print(number)
+
---
+++ HW4 Q8.py
@@ -0,0 +1,5 @@
+tuple1 = (10, 20, 30, 40, 50)
+
+reversed_tuple = tuple1[::-1]
+
+print(reversed_tuple)
